b57c4dcbfc398d163fd4ecb0f36f44a9693c92f0
bati668 <bati@hiroshi.in>
2024-08-20 09:35:22 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -4,25 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "libsnark",
-    // To avoid linker warnings in macOS, make version 13 (or later, 
-    // according to your needs), the minimum supported version:
-    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "libsnark",
-            targets: ["SwiftModule"]
-        ),
+            targets: ["libsnark"])
     ],
     targets: [
         .target(
-            name: "libsnark"
-        ),
-        .target(
-            name: "SwiftModule",
-            dependencies: ["libsnark"],
-            // Specifying the path is superfluous as long as
-            // the corresponding folder is called 'SwiftModule'
-            swiftSettings: [.interoperabilityMode(.Cxx)]
-        ),
+            name: "libsnark",
+            swiftSettings: [.interoperabilityMode(.Cxx)])
     ]
 )
